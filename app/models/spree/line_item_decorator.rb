0aa77ee8a26d9e6634a3fc0dc3ae83a5232aa91d
Allison C. Reilly <acreilly3@gmail.com>
2016-06-24 12:49:11 -0400

    added line item decorator with specs

---
+++ app/models/spree/line_item_decorator.rb
@@ -0,0 +1,25 @@
+Spree::LineItem.class_eval do
+  def to_hash
+    {
+      'Index' => id,
+      'Name' => name,
+      'ItemID' => sku,
+      'Price' => price.to_s,
+      'Qty' => quantity,
+      'TaxCategory' => tax_category
+    }
+  end
+
+  def avatax_cache_key
+    key = ['Spree::LineItem']
+    key << self.id
+    key << self.quantity
+    key << self.price
+    key << self.promo_total
+    key.join('-')
+  end
+
+  def avatax_line_code
+    'LI'
+  end
+end
---
+++ spec/models/spree/line_item_decorator_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe Spree::LineItem, type: :model do
+
+  let(:order) { create :order_with_line_items, line_items_count: 1 }
+  let(:line_item) { order.line_items.first }
+
+  describe '#to_hash' do
+    it 'should create hash of line item information' do
+      expect(line_item.to_hash).to be_kind_of(Hash)
+    end
+    it 'should have index of 1' do
+      response = line_item.to_hash
+      expect(response['Index']).to eq(1)
+    end
+  end
+
+  describe '#avatax_cache_key' do
+    it 'should respond with a cache key' do
+      line_item = create(:line_item, id: 1, quantity: 1, price: 10.0, promo_total: 0.0)
+
+      expected_response = 'Spree::LineItem-1-1-10.0-0.0'
+
+      expect(line_item.avatax_cache_key).to eq(expected_response)
+    end
+  end
+
+  describe '#avatax_line_code' do
+    it 'should equal LI' do
+      expect(line_item.avatax_line_code).to eq('LI')
+    end
+  end
+end
