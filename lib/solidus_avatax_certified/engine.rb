f40be3f3284977cfc8c7d0500a336ec779c21e45
Allison C. Reilly <acreilly3@gmail.com>
2016-06-24 13:11:35 -0400

    added avatax preferences, avatax initializer to set preferences to env variables and support setup

--- lib/solidus_avatax_certified/engine.rb
+++ lib/solidus_avatax_certified/engine.rb
@@ -4,9 +4,29 @@ module SolidusAvataxCertified
     isolate_namespace Spree
     engine_name 'solidus_avatax_certified'
 
+    config.autoload_paths += %W(#{config.root}/lib)
+
     # use rspec for tests
     config.generators do |g|
       g.test_framework :rspec
+    end
+
+    initializer "spree.avatax_certified.preferences", :before => :load_config_initializers do |app|
+      Spree::AppConfiguration.class_eval do
+        preference :avatax_api_username, :string
+        preference :avatax_api_password, :string
+        preference :avatax_company_code, :string
+        preference :avatax_endpoint, :string
+        preference :avatax_account, :string
+        preference :avatax_license_key, :string
+        preference :avatax_iseligible, :boolean, default: true
+        preference :avatax_log, :boolean, default: true
+        preference :avatax_address_validation, :boolean, default: true
+        preference :avatax_address_validation_enabled_countries, :array, default: ["United States", "Canada"]
+        preference :avatax_tax_calculation, :boolean, default: true
+        preference :avatax_document_commit, :boolean, default: true
+        preference :avatax_origin, :string, default: "{}"
+      end
     end
 
     def self.activate
