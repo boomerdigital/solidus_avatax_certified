50224c1ea1684ea8ed394ea856f1f2d9253170fe
Allison C. Reilly <acreilly3@gmail.com>
2016-07-20 15:16:30 -0400

    refactored and fixed indentation to spree use code picker

--- app/assets/javascripts/spree/backend/use_code_picker.js
+++ app/assets/javascripts/spree/backend/use_code_picker.js
@@ -1,54 +1,51 @@
 $.fn.usecodeAutocomplete = function () {
-    'use strict';
+  'use strict';
 
-    this.select2({
-        minimumInputLength: 1,
-        multiple: false,
-        initSelection: function (element, callback) {
-            $.get(Spree.routes.use_code_search, {
-                ids: element.val()
-            }, function (data) {
-                console.log(JSON.stringify(data));
-                callback(data[0]);
-            });
+  this.select2({
+    minimumInputLength: 1,
+    multiple: false,
+    initSelection: function (element, callback) {
+      Spree.ajax({
+        url: Spree.routes.use_code_search,
+        data: {
+          ids: element.val()
         },
-        ajax: {
-            url: Spree.routes.use_code_search,
-            datatype: 'json',
-            data:  function (term) {
-                return {
-                    q: term
-                };
-            },
-            results: function (data) {
-                return {
-                    results: data
-                };
-            }
-        },
-        formatResult: function (use_codes) {
-            if(use_codes.use_code === undefined || use_codes.use_code === ""){
-                return "Enter Avalara Entity Use Code"
-            } else {
-                return use_codes.use_code + ') Description: ' + use_codes.use_code_description;
-            }
-        },
-        formatSelection: function (use_codes) {
-            if(use_codes.use_code === undefined || use_codes.use_code === ""){
-                return "Enter Avalara Entity Use Code"
-            } else {
-                return use_codes.use_code + ') Description: ' + use_codes.use_code_description;
-            }
+        success: function(data) {
+          callback(data[0]);
         }
-    });
-
-    function log(e) {
-        alert(e);
+      })
+    },
+    ajax: {
+      url: Spree.routes.use_code_search,
+      datatype: 'json',
+      data:  function (term) {
+        return {
+          q: term
+        };
+      },
+      results: function (data) {
+        return {
+          results: data
+        };
+      }
+    },
+    formatResult: function (use_codes) {
+      if( !use_codes.use_code ){
+        return "Enter Avalara Entity Use Code"
+      } else {
+        return use_codes.use_code + ') Description: ' + use_codes.use_code_description;
+      }
+    },
+    formatSelection: function (use_codes) {
+      if( !use_codes.use_code ){
+        return "Enter Avalara Entity Use Code"
+      } else {
+        return use_codes.use_code + ') Description: ' + use_codes.use_code_description;
+      }
     }
-
-
+  });
 };
 
 $(document).ready(function () {
-    $('.use_code_picker').usecodeAutocomplete();
+  $('.use_code_picker').usecodeAutocomplete();
 });
