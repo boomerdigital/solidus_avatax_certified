d5631c5c7f249752ba8fb6e465428bd49d83df25
Allison C. Reilly <acreilly3@gmail.com>
2016-08-16 17:27:50 -0400

    Use double quotes instead of single

--- app/assets/javascripts/spree/frontend/address_validator.js
+++ app/assets/javascripts/spree/frontend/address_validator.js
@@ -5,36 +5,36 @@ Spree.ready(function(){
 
 function AddressValidator(){
   this.lineHash = {
-    address1: 'Line1',
-    address2: 'Line2',
-    city: 'City',
-    zipcode: 'PostalCode',
-    country: 'Country',
-    state: 'Region'
+    address1: "Line1",
+    address2: "Line2",
+    city: "City",
+    zipcode: "PostalCode",
+    country: "Country",
+    state: "Region"
   }
 }
 
 AddressValidator.prototype = {
   bindListeners: function(){
-    $('.address_validator').on('click', this.validate.bind(this))
+    $(".address_validator").on("click", this.validate.bind(this))
   },
   validate: function(){
     event.preventDefault();
-    $('#checkout_form_address').validate().form();
+    $("#checkout_form_address").validate().form();
     var address = this.formatAddress();
 
     $.ajax({
       url: Spree.routes.validate_address,
       data: {
         address: address,
-        state: 'address'
+        state: "address"
       }
     }).done(function(data){
       var address = data.Address;
       var controller = this;
       var wrapper = controller.addressWrapper()
 
-      $.each(['Line1', 'Line2', 'City', 'PostalCode'], function(index, value){
+      $.each(["Line1", "Line2", "City", "PostalCode"], function(index, value){
         var keyVal = controller.getKeyByValue(value);
         $(wrapper + " input[id*='" + keyVal + "']").val(address[value])
       }.bind(address))
@@ -47,15 +47,15 @@ AddressValidator.prototype = {
     controller = this;
     var wrapper = controller.addressWrapper();
 
-    $(wrapper + ' input').not("select").each(function(){
-      var id = $(this).attr('id');
-      var line = controller.lineHash[id.split('_').pop()]
+    $(wrapper + " input").not("select").each(function(){
+      var id = $(this).attr("id");
+      var line = controller.lineHash[id.split("_").pop()]
       address[line] = $(this).val()
     })
 
     $(wrapper + " select").each(function(){
-      var id = $(this).attr('id');
-      var line = controller.lineHash[id.slice(0, -3).split('_').pop()]
+      var id = $(this).attr("id");
+      var line = controller.lineHash[id.slice(0, -3).split("_").pop()]
       address[line] = $(this).val()
     })
 
@@ -67,17 +67,17 @@ AddressValidator.prototype = {
   showFlash: function(data){
     var resultCode = data.ResultCode.toLowerCase()
 
-    if(resultCode === 'success') {
-      window.show_flash('success', "Address Validation Successful")
+    if(resultCode === "success") {
+      window.show_flash("success", "Address Validation Successful")
     } else {
-      window.show_flash('error', "Address Validation Error: " + data.Summary)
+      window.show_flash("error", "Address Validation Error: " + data.Summary)
     }
   },
   addressWrapper: function(){
-    if($('#order_use_billing').is(':checked')){
-      return '#billing';
+    if($("#order_use_billing").is(":checked")){
+      return "#billing";
     } else {
-      return '#shipping';
+      return "#shipping";
     }
   }
 }
