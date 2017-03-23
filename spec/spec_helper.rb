e5fd3dbe4c3e1d264326161a4709ab4963388567
Allison C. Reilly <acreilly3@gmail.com>
2017-03-23 15:16:14 -0400

    Clean up spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,12 +22,8 @@ require 'rspec/rails'
 require 'database_cleaner'
 require 'ffaker'
 require 'shoulda/matchers'
+require 'capybara-screenshot/rspec'
 
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
-
-# Requires factories and other useful helpers defined in spree_core.
 require 'spree/testing_support/preferences'
 require 'spree/testing_support/authorization_helpers'
 require 'spree/testing_support/capybara_ext'
@@ -36,9 +32,7 @@ require 'spree/testing_support/factories'
 require 'spree/testing_support/url_helpers'
 require 'spree/testing_support/order_walkthrough'
 
-Dir[File.join(File.dirname(__FILE__), 'factories/*.rb')].each { |f| require f }
 
-require 'capybara-screenshot/rspec'
 Capybara.save_and_open_page_path = ENV['CIRCLE_ARTIFACTS'] if ENV['CIRCLE_ARTIFACTS']
 
 if ENV['WEBDRIVER'] == 'accessible'
@@ -46,59 +40,42 @@ if ENV['WEBDRIVER'] == 'accessible'
   Capybara.javascript_driver = :accessible
 end
 
+Dir[File.join(File.dirname(__FILE__), 'factories/*.rb')].each { |f| require f }
 Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
 
 RSpec.configure do |config|
-  config.include FactoryGirl::Syntax::Methods
-
-  # Infer an example group's spec type from the file location.
   config.infer_spec_type_from_file_location!
+  config.mock_with :rspec
+  config.color = true
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = false
+  config.fail_fast = ENV['FAIL_FAST'] || false
+  config.order = "random"
 
+  config.include FactoryGirl::Syntax::Methods
   config.include Spree::TestingSupport::Preferences
   config.include Spree::TestingSupport::UrlHelpers
   config.include Spree::TestingSupport::ControllerRequests, type: :controller
 
-  # == Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
-  config.mock_with :rspec
-  config.color = true
 
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  # Capybara javascript drivers require transactional fixtures set to false, and we use DatabaseCleaner
-  # to cleanup after each test instead.  Without transactional fixtures set to false the records created
-  # to setup a test will be unavailable to the browser, which runs under a separate server instance.
-  config.use_transactional_fixtures = false
   if ENV['WEBDRIVER'] == 'accessible'
     config.around(:each, :inaccessible => true) do |example|
       Capybara::Accessible.skip_audit { example.run }
     end
   end
 
-  # Ensure Suite is set to use transactions for speed.
   config.before :suite do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with :truncation
   end
 
-  # Before each spec check if it is a Javascript test and switch between using database transactions or not where necessary.
   config.before :each do
     DatabaseCleaner.strategy = RSpec.current_example.metadata[:js] ? :truncation : :transaction
     DatabaseCleaner.start
     MyConfigPreferences.set_preferences
   end
 
-  # After each spec clean the database.
   config.after :each do
     DatabaseCleaner.clean
   end
-
-  config.fail_fast = ENV['FAIL_FAST'] || false
-  config.order = "random"
 end
