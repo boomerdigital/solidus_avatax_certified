69ddba4cf55418eb0d5918f8ff737af5b938b95c
Allison C. Reilly <acreilly3@gmail.com>
2017-04-21 12:27:34 -0400

    Add rescue to Rest Client request in case of timeout

--- app/models/tax_svc.rb
+++ app/models/tax_svc.rb
@@ -62,7 +62,7 @@ class TaxSvc
       http.read_timeout = 1
       request = http.get(uri.request_uri, 'Authorization' => credential)
     rescue => e
-      raise if raise_exceptions?
+      logger.error(e, 'Request Timeout')
     end
 
     response = SolidusAvataxCertified::Response::AddressValidation.new(request.body)
@@ -123,20 +123,24 @@ class TaxSvc
   end
 
   def request(uri, request_hash)
-    res = RestClient::Request.execute(method: :post,
-                                timeout: 1,
-                                open_timeout: 1,
-                                url: service_url + uri,
-                                payload:  JSON.generate(request_hash),
-                                headers: {
-                                  authorization: credential,
-                                  content_type: 'application/json'
-                                }
-    )  do |response, request, result|
-      response
-    end
+    begin
+      res = RestClient::Request.execute(method: :post,
+                                  timeout: 1,
+                                  open_timeout: 1,
+                                  url: service_url + uri,
+                                  payload:  JSON.generate(request_hash),
+                                  headers: {
+                                    authorization: credential,
+                                    content_type: 'application/json'
+                                  }
+      )  do |response, request, result|
+        response
+      end
 
-    JSON.parse(res)
+      JSON.parse(res)
+    rescue => e
+      logger.error(e, 'RestClient')
+    end
   end
 
   def log(method, request_hash = nil)
