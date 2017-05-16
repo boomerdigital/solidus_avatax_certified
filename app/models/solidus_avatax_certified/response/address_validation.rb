792bd6c77a78c877206831aa6730f87a95aba097
Allison Reilly <acreilly3@gmail.com>
2017-05-16 17:25:45 -0400

    If a timeout occurs, the address validator will respond with an error hash

--- app/models/solidus_avatax_certified/response/address_validation.rb
+++ app/models/solidus_avatax_certified/response/address_validation.rb
@@ -3,10 +3,6 @@ module SolidusAvataxCertified
     class AddressValidation < SolidusAvataxCertified::Response::Base
       alias :validation_result :result
 
-      def initialize(result)
-        @result = JSON.parse(result)
-      end
-
       def description
         'Address Validation'
       end
