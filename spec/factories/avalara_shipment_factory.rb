deb5cf4b577e5c6fb239078ee1cf603db5554d1c
Allison Reilly <acreilly3@gmail.com>
2017-07-27 14:21:09 -0400

    Fix deprecation warning by removing add_shipping_method

--- spec/factories/avalara_shipment_factory.rb
+++ spec/factories/avalara_shipment_factory.rb
@@ -10,12 +10,17 @@ FactoryGirl.define do
       tax_included false
     end
 
-    after(:create) do |shipment, evalulator|
-      shipment.add_shipping_method(create(:avalara_shipping_method, tax_included: evalulator.tax_included), true)
+    after(:create) do |shipment, evaluator|
+      shipping_method = create(:avalara_shipping_method, tax_included: evaluator.tax_included)
+      shipment.shipping_rates.create!(
+        shipping_method: shipping_method,
+        cost: evaluator.cost,
+        selected: true
+      )
 
       shipment.order.line_items.each do |line_item|
         line_item.quantity.times do
-          shipment.inventory_units.create(
+          shipment.inventory_units.create!(
             variant_id: line_item.variant_id,
             line_item_id: line_item.id
           )
