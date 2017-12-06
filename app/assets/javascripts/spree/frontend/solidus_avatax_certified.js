7bcfe4b7986c2c63f131430455c2b6ee9a1bc8ef
Allison Reilly <acreilly3@gmail.com>
2017-12-06 13:45:14 +0800

    Refactor address validator, change to coffeescript and pass validator feature specs

--- app/assets/javascripts/spree/frontend/solidus_avatax_certified.js
+++ app/assets/javascripts/spree/frontend/solidus_avatax_certified.js
@@ -1,4 +1,4 @@
-//= require spree/frontend/address_validator
+//= require spree/address_validator.js.coffee
 //= require spree/frontend/flash
 
 Spree.routes.validate_address = Spree.pathFor("checkout/validate_address")
