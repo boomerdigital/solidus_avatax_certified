518d7c6998afc06c49422af9ca35f927ed1f5b52
Allison Reilly <acreilly3@gmail.com>
2017-12-18 21:12:14 +0700

    Get order_id from shipment

--- spec/factories/avalara_shipment_factory.rb
+++ spec/factories/avalara_shipment_factory.rb
@@ -21,7 +21,7 @@ FactoryBot.define do
       shipment.order.line_items.each do |line_item|
         line_item.quantity.times do
           shipment.inventory_units.create!(
-            order_id: line_item.order.id,
+            order: shipment.order_id,
             variant_id: line_item.variant_id,
             line_item_id: line_item.id
           )
