b7265df385dcc7ef882016eb54d38aabb6ae9b32
Alessandro Desantis <desa.alessandro@gmail.com>
2019-08-27 15:21:39 +0200

    Stop using PreferenceSeeder in tests
    The class is outdated, since we don't store preferences in the DB
    anymore. Furthermore, default values are now defined directly in
    Spree::AvataxConfiguration.

--- spec/support/avatax_config_preferences.rb
+++ spec/support/avatax_config_preferences.rb
@@ -2,7 +2,25 @@
 
 class MyConfigPreferences
   def self.set_preferences
-    SolidusAvataxCertified::PreferenceSeeder.seed!(false)
-    Spree::Avatax::Config.origin = "{\"line1\":\"915 S Jackson St\",\"line2\":\"\",\"city\":\"Montgomery\",\"region\":\"AL\",\"postalCode\":\"36104\",\"country\":\"US\"}"
+    Spree::Avatax::Config.reset
+
+    Spree::Avatax::Config.configure do |config|
+      config.company_code = '0' unless config.company_code.present?
+      config.license_key = '12345' unless config.license_key.present?
+      config.account = 'jdoe@example.com' unless config.account.present?
+
+      config.refuse_checkout_address_validation_error = false
+      config.log_to_stdout = false
+      config.raise_exceptions = false
+      config.log = true
+      config.address_validation = true
+      config.tax_calculation = true
+      config.document_commit = true
+      config.customer_can_validate = true
+
+      config.address_validation_enabled_countries = ['United States', 'Canada']
+
+      config.origin = "{\"line1\":\"915 S Jackson St\",\"line2\":\"\",\"city\":\"Montgomery\",\"region\":\"AL\",\"postalCode\":\"36104\",\"country\":\"US\"}"
+    end
   end
 end
