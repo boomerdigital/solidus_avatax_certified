b0f8208f79fe48ae4f8e31350df17e5590944a80
Dumitru Ceban <dumitru@ceban.it>
2019-09-02 18:23:31 +0200

    Deprecate preference seeder
    Since the preferences aren't persisted to DB anymore, the
    PreferenceSeeder.seed! method was only (re)setting the preferences to
    their default values.

--- lib/solidus_avatax_certified/preference_seeder.rb
+++ lib/solidus_avatax_certified/preference_seeder.rb
@@ -4,46 +4,14 @@ module SolidusAvataxCertified
   class PreferenceSeeder
     class << self
       def seed!(print_messages = true)
-        @print_messages = print_messages
-        stored_env_prefs
-        boolean_prefs
-        enabled_countries_pref
-        origin_address
-      end
+        ::Spree::Deprecation.warn(
+          "#{name}#seed! has been deprecated.\n" \
+          "Please refer to our updated setup guide: " \
+          "https://github.com/boomerdigital/solidus_avatax_certified/wiki/Installation"
+        )
 
-      protected
-
-      def stored_env_prefs
-        ::Spree::AvataxConfiguration.storable_env_preferences.each do |env|
-          value = if !ENV["AVATAX_#{env.upcase}"].blank?
-                    ENV["AVATAX_#{env.upcase}"]
-                  end
-
-          ::Spree::Avatax::Config[env.to_sym] = value
-        end
-      end
-
-      def boolean_prefs
-        ::Spree::AvataxConfiguration.boolean_preferences.each do |preference|
-          ::Spree::Avatax::Config[preference.to_sym] = if ['refuse_checkout_address_validation_error', 'log_to_stdout', 'raise_exceptions'].include?(preference)
-                                                       false
-                                                     else
-                                                       true
-                                                     end
-        end
-      end
-
-      def enabled_countries_pref
-        ::Spree::Avatax::Config.address_validation_enabled_countries = ['United States', 'Canada']
-      end
-
-      def origin_address
-        origin = ::Spree::Avatax::Config.origin
-        origin = "{}" if origin.nil?
-      end
-
-      def success_message(name, value)
-        puts "Created: #{name} - #{value || 'Please input value in avalara settings!'}"
+        # maintain the previous behaviour: (re)set the config to default values
+        ::Spree::Avatax::Config.reset
       end
     end
   end
