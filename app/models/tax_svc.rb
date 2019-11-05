54e34180f13d210bbc0f9c5658b57ab394fde86b
Marwan Tanager <marwan.tanager@gmail.com>
2019-11-05 07:42:12 +0200

    PNP-686 Retry tax calculation if it resulted in a negative tax

--- app/models/tax_svc.rb
+++ app/models/tax_svc.rb
@@ -15,7 +15,16 @@ class TaxSvc
       request = { 'error' => { 'message' => e } }
     end
 
-    response = SolidusAvataxCertified::Response::GetTax.new(request)
+    retries = ENV.fetch 'TAX_CALCULATION_RETRIES', 5
+
+    response = loop do
+      r = SolidusAvataxCertified::Response::GetTax.new(request)
+
+      retries -= 1
+
+      break r if retries.zero? || r.faraday.env.body['totalTax'] >= 0
+    end
+
     handle_response(response)
   end
 
