b2ae266815ac148d9280703ee5fc80d8acf7be34
Allison Reilly <acreilly3@gmail.com>
2020-03-30 16:14:15 -0300

    Upgrade to the latest solidus_dev_support

--- lib/solidus_avatax_certified.rb
+++ lib/solidus_avatax_certified.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'solidus'
+require 'solidus_core'
 require 'solidus_support'
 require 'solidus_avatax_certified/engine'
 require 'solidus_avatax_certified/exceptions'
