a1e1f549f29b033fc96a76e1f9db457d3a3939e3
Marc Busqué <marc@lamarciana.com>
2023-06-01 06:30:32 +0200

    Use new recommended way to load factories
    The old way is already deprecated in Solidus v4. See
    https://github.com/solidusio/solidus/pull/5023 for details.
    
    Co-authored-by: Filippo Liverani <dev@mailvore.com>

--- lib/solidus_avatax_certified/testing_support/factories.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-factory_path = "#{File.dirname(__FILE__)}/factories/**/*_factory.rb"
-Dir[factory_path].each { |f| require File.expand_path(f) }
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,7 @@ require 'spree/testing_support/controller_requests'
 
 Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")].each { |f| require f }
 
-require 'solidus_avatax_certified/testing_support/factories'
+SolidusDevSupport::TestingSupport::Factories.load_for(SolidusAvataxCertified::Engine)
 
 RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
